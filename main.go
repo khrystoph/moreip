5c545df10abacbd62496a1028307270fdfa3ef66
Bret Edwards <jbe@amazon.com>
2019-07-01 09:53:52 -0700

    returns request for ip address using remoteaddr

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 //moreip returns your ipv4 or ipv6 address.
-//it also will take you to a random more cowbell meme
 package main
 
 import (
-    "net/http"
-    "fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
+var (
+	Trace         *log.Logger
+	Info          *log.Logger
+	Warning       *log.Logger
+	Error         *log.Logger
+	traceHandle   io.Writer
+	infoHandle    io.Writer = os.Stdout
+	warningHandle io.Writer = os.Stderr
+	errorHandle   io.Writer = os.Stderr
+)
+
+const (
+	cert = "cert.pem"
+	key  = "key.pem"
+)
+
+func init() {
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, req.RemoteAddr)
+	})
+
+	err := http.ListenAndServeTLS(":8443", cert, key, nil)
+	Error.Fatal(err)
+	return
+}
